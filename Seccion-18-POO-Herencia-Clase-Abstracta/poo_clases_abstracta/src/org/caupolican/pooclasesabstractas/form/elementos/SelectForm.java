6d5fc215a8a3a1f477a87a40043ced6ce26c510b
Maximiliano Leiva <apumaleiva@gmail.com>
2025-01-27 13:54:19 -0300

    otra Clase Hija SelectForm

--- Seccion-18-POO-Herencia-Clase-Abstracta/poo_clases_abstracta/src/org/caupolican/pooclasesabstractas/form/elementos/SelectForm.java
+++ Seccion-18-POO-Herencia-Clase-Abstracta/poo_clases_abstracta/src/org/caupolican/pooclasesabstractas/form/elementos/SelectForm.java
@@ -1,24 +1,47 @@
 package org.caupolican.pooclasesabstractas.form.elementos;
 
+import org.caupolican.pooclasesabstractas.form.elementos.select.Option;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SelectForm extends ElementoForm {
 
-    private List<String> options;
+    private List<Option> options;
 
     public SelectForm(String name) {
         super(name);
-        this.options = new ArrayList<String>();
+        this.options = new ArrayList<Option>();
     }
 
-    public SelectForm(String name, List<String> options) {
+    public SelectForm(String name, List<Option> options) {
         super(name);
         this.options = options;
     }
 
+   public SelectForm addOption(Option option) {
+        this.options.add(option);
+        return this; // Return the object itself
+    }
+
     @Override
     public String dibujarHtml() {
-        return "";
+        StringBuilder html = new StringBuilder("<select");
+        html.append(" name='")
+                .append(this.name)
+                .append("'>");
+        for (Option option : options) {
+            html.append("\n<option value='")
+                    .append(option.getValue())
+                    .append("'");
+            if (option.getSelected()) {
+                html.append(" selected");
+            }
+            html.append(">")
+                    .append(option.getName())
+                    .append("</option>");
+        }
+        html.append("\n</select>");
+        return html.toString();
     }
 }
