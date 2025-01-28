b0412445f56e74205ed4ad292ab1a0a1a8239580
Maximiliano Leiva <apumaleiva@gmail.com>
2025-01-27 21:39:28 -0300

    Email validator

---
+++ Seccion-18-POO-Herencia-Clase-Abstracta/poo_clases_abstracta/src/org/caupolican/pooclasesabstractas/form/validador/EmailValidator.java
@@ -0,0 +1,22 @@
+package org.caupolican.pooclasesabstractas.form.validador;
+
+public class EmailValidator extends Validator {
+
+    protected String message = "El formato de email es inválido";
+    private final static String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    
+    @Override
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+
+    @Override
+    public boolean isValid(String value) {
+        return value.matches(EMAIL_REGEX);
+    }
+}
