cf88afcb8525236f34c37c99841d6aae7eb4e252
Maximiliano Leiva <apumaleiva@gmail.com>
2025-01-27 21:49:02 -0300

    Implementando la validacion en la clase abstracta ElementoForm

--- Seccion-18-POO-Herencia-Clase-Abstracta/poo_clases_abstracta/src/org/caupolican/pooclasesabstractas/form/elementos/ElementoForm.java
+++ Seccion-18-POO-Herencia-Clase-Abstracta/poo_clases_abstracta/src/org/caupolican/pooclasesabstractas/form/elementos/ElementoForm.java
@@ -1,11 +1,21 @@
 package org.caupolican.pooclasesabstractas.form.elementos;
 
+import org.caupolican.pooclasesabstractas.form.validador.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 abstract public class ElementoForm {
 
     protected String value; // Protected field porque la clase hija pueda utilizar estos atributos, sin necesidad de implementar el metodo get
     protected String name; // Protected field
 
+    private List<Validator> validators; // Private field
+    private List<String> errors; // Private field
+
     public ElementoForm() {
+        this.validators = new ArrayList<>(); // Initialize the validators list
+        this.errors = new ArrayList<>();// Initialize the errors list
     }
 
     public ElementoForm(String name) {
@@ -13,8 +23,27 @@ abstract public class ElementoForm {
         this.name = name;
     }
 
+    public ElementoForm addValidator(Validator validator) {
+        this.validators.add(validator);
+        return this;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
     public void setValue(String value) {
         this.value = value;
+    }
+
+    public boolean isValid() {
+        this.errors.clear(); // Clear the errors list
+        for (Validator validator : validators) {
+            if (!validator.isValid(this.value)) {
+                this.errors.add(validator.getMessage());
+            }
+        }
+        return this.errors.isEmpty();
     }
 
     abstract public String dibujarHtml(); // Abstract method
