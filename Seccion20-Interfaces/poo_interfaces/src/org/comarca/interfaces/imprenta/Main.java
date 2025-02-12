970e482ea4a9b9c7d124511c778384c2c766467a
Maximiliano Leiva <apumaleiva@gmail.com>
2025-02-12 00:38:22 -0300

    Creando la clase Main

--- Seccion20-Interfaces/poo_interfaces/src/org/comarca/interfaces/imprenta/Main.java
+++ Seccion20-Interfaces/poo_interfaces/src/org/comarca/interfaces/imprenta/Main.java
@@ -1,7 +1,24 @@
 package org.comarca.interfaces.imprenta;
 
+import org.comarca.interfaces.imprenta.modelo.Curriculum;
+import org.comarca.interfaces.imprenta.modelo.Hoja;
+import org.comarca.interfaces.imprenta.modelo.Informe;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Curriculum curriculum = new Curriculum("Caupolican", "Informática", "Estudios en la Universidad de la Vida")
+                .addExperiencia("Programador en Java")
+                .addExperiencia("Programador en Python")
+                .addExperiencia("Programador en C++");
+
+        Informe informe = new Informe("Caupolican", "Colo Colo", "Informe de la reunión de ayer");
+
+        imprimir(curriculum);
+        imprimir(informe);
+    }
+
+    public static void imprimir(Hoja imprimible) {
+        System.out.println(imprimible.imprimir());
     }
 }
