774b98478f18f2e0e42fe370e8b7574e98909944
Maximiliano Leiva <apumaleiva@gmail.com>
2025-02-12 00:33:19 -0300

    Añadiendo nueva clase concreta a la jerarquia

---
+++ Seccion20-Interfaces/poo_interfaces/src/org/comarca/interfaces/imprenta/modelo/Curriculum.java
@@ -0,0 +1,39 @@
+package org.comarca.interfaces.imprenta.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Curriculum extends Hoja {
+
+    private String persona;
+    private String carrera;
+    private List<String> experiencias;
+    public Curriculum(String persona, String carrera, String contenido) {
+        super(contenido);
+        this.persona = persona;
+        this.carrera = carrera;
+        this.experiencias = new ArrayList<>();
+    }
+
+    public Curriculum addExperiencia(String experiencia) {
+        experiencias.add(experiencia);
+        return this; // Devolvemos this para poder encadenar el metodo
+    }
+
+    @Override
+    public String imprimir() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Curriculum de ");
+        sb.append(persona);
+        sb.append(" en ");
+        sb.append(carrera);
+        sb.append(":\n");
+        for (String experiencia : experiencias) {
+            sb.append(" - ");
+            sb.append(experiencia);
+            sb.append("\n");
+        }
+        sb.append(this.contenido);
+        return sb.toString();
+    }
+}
