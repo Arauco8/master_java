1f2844b151b02ae64fb5fd9738e5274854ae159a
Maximiliano Leiva <apumaleiva@gmail.com>
2025-02-12 01:16:08 -0300

    Metodos default y static en interfaces

--- Seccion20-Interfaces/poo_interfaces/src/org/comarca/interfaces/imprenta/modelo/Imprimible.java
+++ Seccion20-Interfaces/poo_interfaces/src/org/comarca/interfaces/imprenta/modelo/Imprimible.java
@@ -2,6 +2,15 @@ package org.comarca.interfaces.imprenta.modelo;
 
 public interface Imprimible {
 
-    public String imprimir(); //los metodos de una interfaz son publicos o default
+    String TEXTO_POR_DEFECTO = "Imprimiendo valor por defecto..."; //por defecto es public static final
+    /*default String imprimir(){//metodo por defecto, solo cuando el metodo sea opcional, cuando sea necesario una implementacion por defecto
+        return "Imprimiendo valor por defecto...";
+    } //los metodos de una interfaz son publicos o default*/
+
+    String imprimir();
+    static void imprimirVacio(){
+        System.out.println(TEXTO_POR_DEFECTO);
+    }
+
 
 }
